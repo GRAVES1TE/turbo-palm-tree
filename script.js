f0c165b647fd9f36749911a28e05de74c958ee1f
GRAVES1TE <antonplotnikovvrn@gmail.com>
2025-06-14 15:43:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
+ymaps.ready(init);
 
-let map, selectedCoords = null, selectedAddress = '';
-
-ymaps.ready(() => {
-  map = new ymaps.Map("map", {
-    center: [55.751244, 37.618423],
-    zoom: 10
+function init() {
+  const map = new ymaps.Map("map", {
+    center: [55.76, 37.64], // Москва
+    zoom: 10,
   });
 
-  new ymaps.SuggestView('search');
+  let placemark;
+  const addressSpan = document.getElementById("selected-address");
 
-  map.events.add('click', function (e) {
-    const coords = e.get('coords');
-    selectedCoords = coords;
-    ymaps.geocode(coords).then(res => {
+  map.events.add("click", function (e) {
+    const coords = e.get("coords");
+
+    if (placemark) {
+      placemark.geometry.setCoordinates(coords);
+    } else {
+      placemark = new ymaps.Placemark(coords, {}, { draggable: true });
+      map.geoObjects.add(placemark);
+    }
+
+    ymaps.geocode(coords).then(function (res) {
       const firstGeoObject = res.geoObjects.get(0);
-      selectedAddress = firstGeoObject.getAddressLine();
-      document.getElementById('address').textContent = 'Выбранный адрес: ' + selectedAddress;
+      const address = firstGeoObject ? firstGeoObject.getAddressLine() : "не удалось определить";
+      addressSpan.textContent = address;
     });
   });
-});
 
-function findAddress() {
-  const query = document.getElementById('search').value;
-  ymaps.geocode(query).then(res => {
-    const firstGeoObject = res.geoObjects.get(0);
-    const coords = firstGeoObject.geometry.getCoordinates();
-    map.setCenter(coords, 16);
-    selectedCoords = coords;
-    selectedAddress = firstGeoObject.getAddressLine();
-    document.getElementById('address').textContent = 'Выбранный адрес: ' + selectedAddress;
+  document.getElementById("confirm-btn").addEventListener("click", function () {
+    alert("Подтвержденный адрес: " + addressSpan.textContent);
   });
-}
-
-function confirmAddress() {
-  if (selectedAddress) {
-    console.log('Подтвержденный адрес:', selectedAddress);
-    alert('Адрес подтвержден: ' + selectedAddress);
-  } else {
-    alert('Сначала выберите адрес на карте.');
-  }
 }
